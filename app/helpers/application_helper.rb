01a6599ddd45ca7a05905e08154916abca8e3c7e
mizokami <suzunatsu@yahoo.com>
2015-03-22 20:31:38 +0900

    タグ出力処理を共通化する

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def dbconsole(sql, result)
+    <<-"HTML".strip_heredoc.html_safe
+<pre class="code"><code>#{h sql.chomp}
+
+#{h Hirb::Helpers::AutoTable.render(result)}</code></pre>
+    HTML
+  end
 end
